4b2a5a6724392cbc492dc50182c4943986506f8d
TheDarklightsun <bublik.kirill2012@gmail.com>
2023-11-16 04:41:16 +0200

    add-auth

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,53 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHandler, HttpInterceptor, HttpParams, HttpRequest} from "@angular/common/http";
+
+import {Recipe} from "../recipes/recipe.model";
+import {RecipeService} from "../recipes/recipe.service";
+import {exhaustMap, map, take, tap} from "rxjs";
+import {AuthService} from "../auth/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService implements HttpInterceptor {
+  constructor(private http: HttpClient,
+              private recipeService: RecipeService,
+              private authService: AuthService
+  ) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(req);
+  }
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+
+    this.http
+      .put(
+        'https://ng-recipe-book-a9c4d-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    return this.http.get<Recipe[]>(
+      'https://ng-recipe-book-a9c4d-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+    ).pipe(
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
+  }
+}
