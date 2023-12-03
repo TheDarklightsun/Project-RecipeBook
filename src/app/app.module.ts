e3da05b2bded674db3fb808cf9422e60a7b262a7
TheDarklightsun <bublik.kirill2012@gmail.com>
2023-12-04 01:42:42 +0200

    add-login-effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import {NgOptimizedImage} from "@angular/common";
 import {StoreModule} from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
@@ -10,7 +11,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core.module';
 import * as fromApp from './store/app.reducer';
-import { EffectsModule } from '@ngrx/effects';
+import {AuthEffects} from "./auth/store/auth.effects";
 
 @NgModule({
   declarations: [
@@ -23,9 +24,9 @@ import { EffectsModule } from '@ngrx/effects';
     AppRoutingModule,
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([AuthEffects]),
     SharedModule,
-    CoreModule,
-    EffectsModule.forRoot([])
+    CoreModule
   ],
   bootstrap: [AppComponent],
   // providers: [LoggingService]
