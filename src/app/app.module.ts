16352d9016b20ff803a9a1de1190c0d5e38ca14e
TheDarklightsun <bublik.kirill2012@gmail.com>
2023-12-05 23:41:42 +0200

    add-devtools-and-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import {NgOptimizedImage} from "@angular/common";
 import {StoreModule} from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
@@ -12,6 +13,8 @@ import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core.module';
 import * as fromApp from './store/app.reducer';
 import {AuthEffects} from "./auth/store/auth.effects";
+import {environment} from "../environments/environment";
+import {StoreRouterConnectingModule} from "@ngrx/router-store";
 
 @NgModule({
   declarations: [
@@ -25,6 +28,8 @@ import {AuthEffects} from "./auth/store/auth.effects";
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
+    StoreRouterConnectingModule.forRoot(),
     SharedModule,
     CoreModule
   ],
